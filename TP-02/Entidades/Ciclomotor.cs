419cb33569df14171266cbe81d942fca26a5b50e
Brandon Suarez <brandonagustinsuarez11@gmail.com>
2022-05-02 10:01:43 -0300

    TP 2

---
+++ TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        /// <summary>
+        /// Constructor: inicializa los atributos de la clase base y la clase heredada.
+        /// </summary>
+        /// <param name="marca">recibe la marca</param>
+        /// <param name="chasis">recibe el chasis</param>
+        /// <param name="color">recibe el color</param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+            this.chasis = chasis;
+            this.color = color;
+        }
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+        /// <summary>
+        /// Muestra los elementos.
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
